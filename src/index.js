dfb8ce8bffce15cc3619358ca117a197fda672cc
Quoc Duong PHUNG <qdphung19@gmail.com.com>
2022-04-26 01:01:44 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/data-viz-d3js">
             <App />
         </BrowserRouter>
     </React.StrictMode>,
